666fe186bb9959572eefae0a19558165de795a9b
Matt Jones <bigboxer23@gmail.com>
2025-02-01 13:55:09 -0600

    feat: refactor to use slf4j annoation for logs

--- src/main/java/com/bigboxer23/garage/services/CommunicationService.java
+++ src/main/java/com/bigboxer23/garage/services/CommunicationService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class CommunicationService extends BaseService {
+
 	@Value("${GarageOpenUrl}")
 	private String kOpenUrl;
 
